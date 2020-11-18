231afa242c4b5e50aef80455ed970a09b4c73c99
Clay Hoover <ClayHoover@Clays-MBP.attlocal.net>
2020-11-18 09:38:18 -0500

    develop branch

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+
+function Footer() {
+    return(
+    <footer>
+    <p>Copyright Clay Hoover {year}</p>
+    </footer>
+    )
+    };
+
+    export default Footer;
---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+    return(
+  <div className="note">
+      <h1 contentEditable="true">Title</h1>
+      <p contentEditable="true">Content</p>
+  </div>
+    )
+};
+
+export default Note;
